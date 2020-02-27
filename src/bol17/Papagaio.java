ea6bb826f25407916b0df00fd2c4d11beb51dc4f
ceque <ceque@jalvarezbarciela>
2020-02-27 12:55:18 +0100

    Boletin17

---
+++ src/bol17/Papagaio.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol17;
+
+/**
+ *
+ * @author ceque
+ */
+public class Papagaio extends Ave implements IPodeVoar {
+
+    @Override
+    public void voar() {
+        System.out.println("Pode voar");
+    }
+    
+}
